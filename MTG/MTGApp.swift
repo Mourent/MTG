4f15797e9f171d95597bed13982802734d98d05f
Mourent <jmourent@student.ciputra.ac.id>
2023-11-10 08:01:54 +0700

    Initial Commit

---
+++ MTG/MTGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGApp.swift
+//  MTG
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MTGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
